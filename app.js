96648ec2e6af6348de58ef1745b512c7d4b3be82
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-01-08 15:15:29 -0430

    removing dumb test

--- app.js
+++ app.js
@@ -3,40 +3,40 @@ var cronJob = require('cron').CronJob,
     D  = require('./disqus-config.js');
 
 var lastTimestamp = null;
-hipqus.sendMessage('Test without room token');
-// new cronJob('*/10 * * * * *', function (){
 
-//     D.disqus.request('posts/list', D.disqus_options, function(data) {
+new cronJob('*/10 * * * * *', function (){
 
-//         if(data.error){
-//             console.log('Something went wrong...');
-//             console.log(data);
-//         }else{
-//             var response = JSON.parse(data).response;
+    D.disqus.request('posts/list', D.disqus_options, function(data) {
 
-//             if(!lastTimestamp){
-//                 // initializing lastTimestamp with most recent comment date
-//                 lastTimestamp = new Date(response[0].createdAt);
-//             }else{
-//                 var mostRecentTimestamp = null;
-//                 for(var i=0; i < response.length; i++){
-//                     var postDate = new Date(response[i].createdAt);
-//                     if(postDate > lastTimestamp){
-//                         hipqus.buildMessage(response[i]);
-//                         if(!mostRecentTimestamp){
-//                             mostRecentTimestamp = postDate;
-//                         }
-//                     }else{
-//                         console.log('Nothing new to send. Last comment timestamp: ' + lastTimestamp.toISOString());
-//                         break;
-//                     }
-//                 }
-//                 if(mostRecentTimestamp){
-//                     lastTimestamp = mostRecentTimestamp;
-//                 }
-//             }
-//         }
-//     });
+        if(data.error){
+            console.log('Something went wrong...');
+            console.log(data);
+        }else{
+            var response = JSON.parse(data).response;
 
-// }, null, true, null);
+            if(!lastTimestamp){
+                // initializing lastTimestamp with most recent comment date
+                lastTimestamp = new Date(response[0].createdAt);
+            }else{
+                var mostRecentTimestamp = null;
+                for(var i=0; i < response.length; i++){
+                    var postDate = new Date(response[i].createdAt);
+                    if(postDate > lastTimestamp){
+                        hipqus.buildMessage(response[i]);
+                        if(!mostRecentTimestamp){
+                            mostRecentTimestamp = postDate;
+                        }
+                    }else{
+                        console.log('Nothing new to send. Last comment timestamp: ' + lastTimestamp.toISOString());
+                        break;
+                    }
+                }
+                if(mostRecentTimestamp){
+                    lastTimestamp = mostRecentTimestamp;
+                }
+            }
+        }
+    });
+
+}, null, true, null);
 
